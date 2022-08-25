4a857d6773c1b176b88ee760ea299f82d4e15ada
Nick <108188470+nickhallden1330@users.noreply.github.com>
2022-08-25 16:11:16 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var firstName = prompt('Ismingizni kiriting?' + ' ' + 'Nick');
+var question = prompt('css so"zini ma"nosi?' + ' ' + 'A)cascading style sheets' + ' ' + 'B)creating style sheets' + ' ' + 'C)creator style sheets');
+var question1 = prompt('Nechi xil style berish usuli bor?' + ' ' + 'A)4-xil' + ' ' + 'B)5-xil' + ' ' + 'C)3-xil')
+var question2 = prompt('ul bilan olni farqi?' + ' ' + 'A)sonlar bilan stillarda' + ' ' + 'B)tartibli, tartibsiz' + ' ' + 'C)ikkalasi');
+var question3 = prompt('sub pasda chiqadimi yoki tepadami?' + ' ' + 'A)tepada' + ' ' + 'B)pasda' + ' ' + 'C)bilmiman😌');
+var question4 = prompt('cite atrebuti qaysi tegda bor?' + ' ' + 'A)blockquote' + ' ' + 'B)q' + ' ' + 'C)ikkalovida');
+var question5 = prompt('a tegini ichida mail yozish mumkinmi?' + ' ' + 'A)ha' + ' ' + 'B)yoq' + ' ' + 'C)bimasam🤔');
+var question6 = prompt('a tegini ichida target nma uchun yoziladi?' + ' ' + 'A)alohida page😅ochish uchun' + ' ' + 'B)pageni😅yopish uchun' + ' ' + 'C)bimasam🤔');
+var question7 = prompt('time tegini ichida qaysi attrebut qo"llaniladi?' + ' ' + 'A)time set' + ' ' + 'B)time' + ' ' + 'C)datetime');
+var question8 = prompt('Rasmlarni nechta formatlari bor?' + ' ' + 'A)4-ta' + ' ' + 'B)5-ta' + ' ' + 'C)3-ta');
+var question9 = prompt('DRY nma?' + ' ' + 'A)принцип😏' + ' ' + 'B)soz' + ' ' + 'C)gap');
+
+console.log(firstName);
+console.log('To"g"ri javob a' + question);
+console.log('To"g"ri javob b' + question1);
+console.log('To"g"ri javob b' + question2);
+console.log('To"g"ri javob c' + question3);
+console.log('To"g"ri javob c' + question4);
+console.log('To"g"ri javob b' + question5);
+console.log('To"g"ri javob a' + question6);
+console.log('To"g"ri javob c' + question7);
+console.log('To"g"ri javob c' + question8);
+console.log('To"g"ri javob a' + question9);
